83c50f5aac06bff4fcaf734d313b694b206363df
BÜŞRA KURT <busrakurt26082@gmail.com>
2025-12-06 18:29:48 +0300

    Java video oynatma listesi url hazırlandı

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,242 @@
+-- =========================
+-- JAVA - BEGINNER DERSLER
+-- =========================
+--başlık, açıklama, youtube_url, sıra_indeksi, etkin, dil, seviye
+INSERT INTO lessons (title, description, youtube_url, order_index, active, language, level)
+VALUES
+  ('Giriş ve Tanıtım', 
+   'Java nedir, hangi alanlarda kullanılır?', 
+   'https://www.youtube.com/watch?v=a8Fe2qbnYDM&list=PLqG356ExoxZUGwbqoJEKSMnaxVJe4Uvf8&index=2&t=3s', 
+   1, true, 'JAVA', 'BEGINNER'),
+
+  ('Hangi IDE’yi Kullanıyoruz?', 
+   'IDE Seçimi', 
+   'https://www.youtube.com/watch?v=joOg5R_EwBk&list=PLqG356ExoxZUGwbqoJEKSMnaxVJe4Uvf8&index=3', 
+   2, true, 'JAVA', 'BEGINNER'),
+
+  ('Java JDK / Java SE 11 SDK kurulumu 1', 
+   'IDE KURULUMU', 
+   'https://www.youtube.com/watch?v=E9Xc1PK0kp4&list=PLqG356ExoxZUGwbqoJEKSMnaxVJe4Uvf8&index=4', 
+   3, true, 'JAVA', 'BEGINNER'),
+   
+   ('IntelliJ Idea Kurulumu', 
+   'IntelliJ Idea Kurulumu', 
+   'https://www.youtube.com/watch?v=B_1HxhAYvVg&list=PLqG356ExoxZUGwbqoJEKSMnaxVJe4Uvf8&index=5', 
+   4, true, 'JAVA', 'BEGINNER'),
+
+  ('Hello World Uygulaması ile Başlayalım', 
+   'İlk Java programı (Hello World / ilk konsol çıktısı)', 
+   'https://www.youtube.com/watch?v=dtP6yK50xIs&list=PLqG356ExoxZUGwbqoJEKSMnaxVJe4Uvf8&index=6', 
+   5, true, 'JAVA', 'BEGINNER'),
+
+  ('Değişkenler ve Değişken Kullanım İhtiyacı', 
+   'Değişkenler ve Değişken Kullanım İhtiyacı', 
+   'http://youtube.com/watch?v=LZqATw5PK2o&list=PLqG356ExoxZUGwbqoJEKSMnaxVJe4Uvf8&index=7', 
+   6, true, 'JAVA', 'BEGINNER'),
+   
+   ('Temel Veri Tipleriyle Çalışmak', 
+   'Temel Veri Tipleri', 
+   'https://www.youtube.com/watch?v=McySkAILr6s&list=PLqG356ExoxZUGwbqoJEKSMnaxVJe4Uvf8&index=8', 
+   7, true, 'JAVA', 'BEGINNER'),
+
+  ('If Bloklarıyla Çalışmak', 
+   'If Yapısı', 
+   'https://www.youtube.com/watch?v=jqRfNzwCsF8&list=PLqG356ExoxZUGwbqoJEKSMnaxVJe4Uvf8&index=9', 
+   8, true, 'JAVA', 'BEGINNER'),
+
+  ('ReCap Demo 1 En Büyük Sayı Hangisi', 
+   'Örnek', 
+   'https://www.youtube.com/watch?v=Un3eBVe8lFY&list=PLqG356ExoxZUGwbqoJEKSMnaxVJe4Uvf8&index=10', 
+   9, true, 'JAVA', 'BEGINNER'),
+
+('Switch Bloklarıyla Çalışmak', 
+   'Switch Yapısı', 
+   'https://www.youtube.com/watch?v=eoWiaZ2lFgw&list=PLqG356ExoxZUGwbqoJEKSMnaxVJe4Uvf8&index=11', 
+   10, true, 'JAVA', 'BEGINNER');
+   
+   
+   -- =========================
+-- JAVA - INTERMEDIATE DERSLER
+-- =========================
+INSERT INTO lessons (title, description, youtube_url, order_index, active, language, level)
+VALUES
+  ('For Döngüsüyle Çalışmak', 
+   'For Döngüsünü Anlamak', 
+   'https://www.youtube.com/watch?v=oXnUG0e8fVk&list=PLqG356ExoxZUGwbqoJEKSMnaxVJe4Uvf8&index=12', 
+   11, true, 'JAVA', 'INTERMEDIATE'),
+
+  ('While Döngüsüyle Çalışmak', 
+   'While Döngüsünü Anlamak', 
+   'https://www.youtube.com/watch?v=_2jRJPFaVN0&list=PLqG356ExoxZUGwbqoJEKSMnaxVJe4Uvf8&index=13', 
+  12, true, 'JAVA', 'INTERMEDIATE'),
+
+  ('Do-While Döngüsüyle Çalışmak', 
+   'Do-While Döngüsünü Anlamak', 
+   'https://www.youtube.com/watch?v=mtjAXipAo20&list=PLqG356ExoxZUGwbqoJEKSMnaxVJe4Uvf8&index=14', 
+   13, true, 'JAVA', 'INTERMEDIATE'),
+   
+   ('Dizilerle Çalışmaya Başlamak', 
+   'Diziler', 
+   'https://www.youtube.com/watch?v=AvtshUX9gHE&list=PLqG356ExoxZUGwbqoJEKSMnaxVJe4Uvf8&index=15', 
+   14, true, 'JAVA', 'INTERMEDIATE'),
+
+  ('ReCap Demo 2-Dizilerle Çalışmak', 
+   'Örnek', 
+   'https://www.youtube.com/watch?v=83_X17EZ4XI&list=PLqG356ExoxZUGwbqoJEKSMnaxVJe4Uvf8&index=16', 
+  15, true, 'JAVA', 'INTERMEDIATE'),
+
+  ('Çok Boyutlu Dizilerle Çalışmak', 
+   'Çok boyutlu diziler(matris, tablo, oyun haritası)', 
+   'https://www.youtube.com/watch?v=G3EEte233QM&list=PLqG356ExoxZUGwbqoJEKSMnaxVJe4Uvf8&index=17', 
+   16, true, 'JAVA', 'INTERMEDIATE'),
+   
+   ('Stringlerle Çalışmak 1 ', 
+   'String Yapısı', 
+   'https://www.youtube.com/watch?v=_H8PoxHQibE&list=PLqG356ExoxZUGwbqoJEKSMnaxVJe4Uvf8&index=18', 
+   17, true, 'JAVA', 'INTERMEDIATE'),
+
+  ('Stringlerle Çalışmak 2', 
+   'String Yapısı', 
+   'https://www.youtube.com/watch?v=4ODOWQ1tZgM&list=PLqG356ExoxZUGwbqoJEKSMnaxVJe4Uvf8&index=19', 
+  18, true, 'JAVA', 'INTERMEDIATE'),
+
+  ('Mini Proje 1 - Sayı Asal mı?', 
+   'Mini Proje 1', 
+   'https://www.youtube.com/watch?v=3uU8WaTssVg&list=PLqG356ExoxZUGwbqoJEKSMnaxVJe4Uvf8&index=20', 
+   19, true, 'JAVA', 'INTERMEDIATE'),
+
+  ('Kalın Sesli ve İnce Sesli Harfler', 
+   'Mini Proje 2', 
+   'https://www.youtube.com/watch?v=urX1HV8MT7g&list=PLqG356ExoxZUGwbqoJEKSMnaxVJe4Uvf8&index=21', 
+   20, true, 'JAVA', 'INTERMEDIATE'),
+   
+     ('Mükemmel Sayılar', 
+   'Mini Proje 3 ', 
+   'https://www.youtube.com/watch?v=G1Vy3AYUg5Y&list=PLqG356ExoxZUGwbqoJEKSMnaxVJe4Uvf8&index=22', 
+   21, true, 'JAVA', 'INTERMEDIATE'),
+  
+     ('Arkadaşlar Sayılar', 
+   'Mini Proje 4 ', 
+   'https://www.youtube.com/watch?v=35wQMuT-2Z0&list=PLqG356ExoxZUGwbqoJEKSMnaxVJe4Uvf8&index=23', 
+   22, true, 'JAVA', 'INTERMEDIATE'),
+   
+   ('Sayı Bulma', 
+   'Mini Proje 5', 
+   'https://www.youtube.com/watch?v=MaJXZn3CqVA&list=PLqG356ExoxZUGwbqoJEKSMnaxVJe4Uvf8&index=24', 
+   23, true, 'JAVA', 'INTERMEDIATE'),
+   
+     ('Metodları Anlamak', 
+   'Metodlar ', 
+   'https://www.youtube.com/watch?v=XsIJn8pjdOM&list=PLqG356ExoxZUGwbqoJEKSMnaxVJe4Uvf8&index=25', 
+   24, true, 'JAVA', 'INTERMEDIATE'),
+   
+     ('Parametreli Metodları Anlamak', 
+   'Parametreli Metodlar ', 
+   'https://www.youtube.com/watch?v=VszZhqQ-jTM&list=PLqG356ExoxZUGwbqoJEKSMnaxVJe4Uvf8&index=26', 
+   25, true, 'JAVA', 'INTERMEDIATE'),
+   
+      ('Variable Arguments ile Çalışmak', 
+   'Variable Arguments', 
+   'https://www.youtube.com/watch?v=f4r1T72OyTA&list=PLqG356ExoxZUGwbqoJEKSMnaxVJe4Uvf8&index=27', 
+   26, true, 'JAVA', 'INTERMEDIATE');
+
+
+   -- =========================
+-- JAVA - ADVANCED DERSLER
+-- =========================
+INSERT INTO lessons (title, description, youtube_url, order_index, active, language, level)
+VALUES
+  ('Class Nedir?-Doğru Anlamak', 
+   'Class Yapısı', 
+   'https://www.youtube.com/watch?v=zfIt6x-guc0&list=PLqG356ExoxZUGwbqoJEKSMnaxVJe4Uvf8&index=28', 
+   27, true, 'JAVA', 'ADVANCED'),
+
+  ('Bir Class Nasıl Çalışır', 
+   'Class Çalışma Mantığı', 
+   'https://www.youtube.com/watch?v=v8JMot51Uqk&list=PLqG356ExoxZUGwbqoJEKSMnaxVJe4Uvf8&index=29', 
+   28, true, 'JAVA', 'ADVANCED'),
+
+  ('Çok Önemli -Değer ve Referans Tipleri Anlamak', 
+   '*****', 
+   'https://www.youtube.com/watch?v=y9vmseRAMiU&list=PLqG356ExoxZUGwbqoJEKSMnaxVJe4Uvf8&index=30', 
+   29, true, 'JAVA', 'ADVANCED');
+   
+   ('ReCap Demo Classlarla Çalışmak', 
+   'Demo Classlarla Çalışmak', 
+   'https://www.youtube.com/watch?v=soJKdeo6JQg&list=PLqG356ExoxZUGwbqoJEKSMnaxVJe4Uvf8&index=31', 
+   30, true, 'JAVA', 'ADVANCED'),
+
+  ('Field ve Attribute ile Çalışmak', 
+   'Field ve Attribute yapısını anlamak', 
+   'https://www.youtube.com/watch?v=EoXqaSX9nrc&list=PLqG356ExoxZUGwbqoJEKSMnaxVJe4Uvf8&index=32', 
+   31, true, 'JAVA', 'ADVANCED'),
+
+  ('Encapsulation İhityacını Anlamak', 
+   'Encapsulation Gizli Dünyaya Açılan Kapı', 
+   'https://www.youtube.com/watch?v=9qYgEaAaef8&list=PLqG356ExoxZUGwbqoJEKSMnaxVJe4Uvf8&index=33', 
+   32, true, 'JAVA', 'ADVANCED');
+   
+   ('Getter ve Setter ile Çalışmak', 
+   'Getter ve Setter', 
+   'https://www.youtube.com/watch?v=GDZmePf3Xmo&list=PLqG356ExoxZUGwbqoJEKSMnaxVJe4Uvf8&index=34', 
+   33, true, 'JAVA', 'ADVANCED'),
+
+  ('Constructor ile Çalışmak', 
+   'Constructorı Anlamak', 
+   'https://www.youtube.com/watch?v=HDaM5aRjWvI&list=PLqG356ExoxZUGwbqoJEKSMnaxVJe4Uvf8&index=35', 
+   34, true, 'JAVA', 'ADVANCED'),
+
+  ('Method Overloading ile Çalışmak', 
+   'Method Overloading', 
+   'https://www.youtube.com/watch?v=X1UnM4W4-_w&list=PLqG356ExoxZUGwbqoJEKSMnaxVJe4Uvf8&index=36', 
+   35, true, 'JAVA', 'ADVANCED'),
+   
+     ('Inheritance Yapısını Anlamak', 
+   'Inheritance Yapısı', 
+   'https://www.youtube.com/watch?v=SYLUS_xInVA&list=PLqG356ExoxZUGwbqoJEKSMnaxVJe4Uvf8&index=37', 
+   36, true, 'JAVA', 'ADVANCED'),
+
+  ('Önemli-Inheritance Demo', 
+   'Inheritance', 
+   'https://www.youtube.com/watch?v=N6Na8LLl-OQ&list=PLqG356ExoxZUGwbqoJEKSMnaxVJe4Uvf8&index=38', 
+   37, true, 'JAVA', 'ADVANCED'),
+
+  ('Gerçek Hayatta Polimorfizm ile Çalışmak', 
+   '**********', 
+   'https://www.youtube.com/watch?v=DfEFbI6z2Is&list=PLqG356ExoxZUGwbqoJEKSMnaxVJe4Uvf8&index=39', 
+   38, true, 'JAVA', 'ADVANCED');
+   
+   ('Overriding Altyapısıyla Çalışmak', 
+   'Override', 
+   'https://www.youtube.com/watch?v=P4j2oh84ht4&list=PLqG356ExoxZUGwbqoJEKSMnaxVJe4Uvf8&index=40', 
+   39, true, 'JAVA', 'ADVANCED'),
+
+  ('Abstract Sınıflarla Çalışmak', 
+   'Abstract Yapısını Anlamak', 
+   'https://www.youtube.com/watch?v=7PJ6YCX52XA&list=PLqG356ExoxZUGwbqoJEKSMnaxVJe4Uvf8&index=41', 
+   40, true, 'JAVA', 'ADVANCED'),
+   
+    ('Demo-Abstract Sınıflarını Pekiştirmek', 
+   'Demo-Abstract', 
+   'https://www.youtube.com/watch?v=U1sapsgPEvU&list=PLqG356ExoxZUGwbqoJEKSMnaxVJe4Uvf8&index=42', 
+   41, true, 'JAVA', 'ADVANCED');
+   
+   ('Interfaceler ile Çalışmak', 
+   'Kurallar Bütünü', 
+   'https://www.youtube.com/watch?v=Cn2P_r_oLmk&list=PLqG356ExoxZUGwbqoJEKSMnaxVJe4Uvf8&index=43', 
+   42, true, 'JAVA', 'ADVANCED'),
+
+  ('Interface ile Polymorphism', 
+   'Interface ile Polymorphism', 
+   'https://www.youtube.com/watch?v=vJ3UkzENgHA&list=PLqG356ExoxZUGwbqoJEKSMnaxVJe4Uvf8&index=44', 
+   43, true, 'JAVA', 'ADVANCED'),
+
+    ('Demo:Çoklu Implementasyonu Kavramak', 
+   'Demo:Çoklu Implementasyonu Kavramak', 
+   'https://www.youtube.com/watch?v=NX1IjYKNhbU&list=PLqG356ExoxZUGwbqoJEKSMnaxVJe4Uvf8&index=45', 
+   44, true, 'JAVA', 'ADVANCED'),
+
+  ('Composition-Inner Class ve Static', 
+   'Composition-Inner Class ve Static', 
+   'https://www.youtube.com/watch?v=CRGYJ_kJpPk&list=PLqG356ExoxZUGwbqoJEKSMnaxVJe4Uvf8&index=46', 
+   45, true, 'JAVA', 'ADVANCED');
